7fbc3845eb55f29268536cc4a5d7d2b33229bd83
botwhatsapp1234 <108470542+botwhatsapp1234@users.noreply.github.com>
2022-07-06 16:49:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)═══════════════════════════════════════//
 //
-//                              K.PRABHASHA
+//                              yugith
 //𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
 //
 //════════════(00)════════════════//
@@ -20,15 +20,15 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is yugith🔥
+My Creator is yugith minlaka😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+940758525052'] //Owner number in aive msg
 global.premium = ['+940758525052'] //Owner Number info
 global.ownernomer = '+940758525052' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎yugith' //Owner Name
+global.botname = ' ☬yugith bot' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
